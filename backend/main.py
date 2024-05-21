5172d205bbf83f17487547eeb80992f35cd71b12
JayO-1 <jozuzu1@yahoo.com>
2024-05-21 18:45:05 +0100

    Ex2.1 and 2.2: Create api routes with desired functionality

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from enum import Enum
+from datetime import datetime
 
 
 app = FastAPI()
@@ -12,7 +15,42 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class Status(Enum):
+    SUBMITTED="submitted"
+    PROCESSING="processing"
+    COMPLETE="complete"
+    
+class Case(BaseModel):
+    id: str
+    created_at: datetime
+    status: str
+
+id = 0
+cases = {
+    # case_{id}: Case(id="case_{id}", created_at=datetime.now(), status=Status.SUBMITTED),
+}
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/cases/{case_id}")
+def query_case_by_id(case_id: str):
+    if case_id not in cases:
+        HTTPException(status_code=404, detail=f"Case with {case_id=} does not exist.")
+    
+    return cases[case_id]
+
+@app.get("/cases")
+def get_all_cases() -> dict[str, dict[int, Case]]:
+    return { "cases": cases }
+
+@app.post("/cases")
+def create_case() -> dict[str, str]:
+    case_id = f"case_{id}"
+    cases[case_id] = Case(id = case_id, created_at=datetime.now(), status=Status.SUBMITTED)
+    response = { "case_id": case_id }
+    id += 1
+    
+    return response
+    
