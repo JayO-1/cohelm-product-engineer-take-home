d3bb136a2e5689655cf5cf885ed7f3c3871543ce
JayO-1 <jozuzu1@yahoo.com>
2024-05-25 16:19:58 +0100

    Ex3: Added audio player functionality to summary

---
+++ frontend/components/dashboard/case-details/summary/audio-player.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { MdVolumeUp, MdVolumeOff } from 'react-icons/md';
+
+export default function AudioPlayer({ isMet, summary }) {
+    const [isSpeaking, setIsSpeaking] = useState(false);
+    const synthesis = window.speechSynthesis;
+    let utterance;
+
+    const toggleSpeech = () => {
+        if (isSpeaking) {
+            synthesis.cancel();
+        } else if (summary !== '') {
+            utterance = new SpeechSynthesisUtterance(summary);
+            
+            utterance.onstart = () => {
+                setIsSpeaking(true);
+            };
+
+            utterance.onend = () => {
+                setIsSpeaking(false);
+            };
+
+            utterance.onerror = () => {
+                setIsSpeaking(false);
+            };
+
+            synthesis.speak(utterance);
+        }
+    };
+
+    useEffect(() => {
+        return () => {
+            if (isSpeaking) {
+                synthesis.cancel();
+            }
+        };
+    }, [isSpeaking, synthesis]);
+
+    return (
+        <button onClick={ toggleSpeech }>
+            {isSpeaking ? (
+                <MdVolumeOff className={`text-lg ${isMet ? 'text-green-600' : 'text-red-600'}`} />
+            ) : (
+                <MdVolumeUp className={`text-lg ${isMet ? 'text-green-600' : 'text-red-600'}`} />
+            )}
+        </button>
+    )
+}
--- frontend/components/dashboard/case-details/summary/index.tsx
+++ frontend/components/dashboard/case-details/summary/index.tsx
@@ -1,12 +1,13 @@
-'use client'
-
-import { useEffect } from 'react';
 import Markdown from 'react-markdown';
+import AudioPlayer from './audio-player';
 
-export default function Summary( { isMet, summary }) {
+export default function Summary({ isMet, summary }) {
     return (
         <div className={`w-[75%] rounded-lg px-4 py-2 ${ isMet ? 'bg-green-100' : 'bg-red-100' }`}>
-            <h2 className={`font-bold ${ isMet ? 'text-green-600' : 'text-red-600' }`}>Summary</h2>
+            <span className="flex justify-between items-center">
+                <h2 className={`font-bold ${ isMet ? 'text-green-600' : 'text-red-600' }`}>Summary</h2>
+                <AudioPlayer isMet={ isMet } summary={ summary } />
+            </span>
             <Markdown className="markdown mt-4 text-md">{summary}</Markdown>
         </div>
     )
