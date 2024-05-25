3a8d502f8afd25cb7a408e5611ee11047260e9e2
JayO-1 <jozuzu1@yahoo.com>
2024-05-25 17:41:18 +0100

    Ex3: Add input tag to file upload

--- frontend/components/guidelines-upload/index.tsx
+++ frontend/components/guidelines-upload/index.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { useDashboard } from "@/context/dashboard-context";
-import classNames from "classnames";
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { FaCheck } from "react-icons/fa";
 import { MdOutlineFileUpload } from "react-icons/md";
 import Spinner from "@/components/dashboard/spinner";
@@ -11,34 +10,49 @@ import toast, { Toaster } from 'react-hot-toast';
 export default function GuidelinesUpload() {
     const { medicalRecord, guidelinesFile, setGuidelinesFile } = useDashboard();
     const [isUploading, setIsUploading] = useState(false);
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
 
     const handleClick = () => {
         if (medicalRecord == null) {
-            return toast.error("Upload medical record first!")
+            return toast.error("Upload medical record first!");
         }
+        
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    }
 
+    const handleFileChange = () => {
         setIsUploading(true);
         
         setTimeout(() => {
             setIsUploading(false);
             setGuidelinesFile({ url: "/assets/guidelines.pdf" });
         }, 3000);
-    }
+    };
 
     return(
         <div className="w-full h-64 flex flex-col items-center justify-center gap-4">
             <h2 className="self-start font-medium text-xl">Upload Guidelines</h2>
             <div className="w-full h-full bg-gray-100 rounded-3xl border-4 border-black-500 border-dashed flex flex-row items-center justify-center">
                 {!isUploading && guidelinesFile === null && (
-                    <button
-                        className="flex flex-col items-center justify-center font-medium text-lg"
-                        onClick={handleClick}
-                    >
-                        <MdOutlineFileUpload className="text-5xl text-blue-500 animate-bounce" />
-                        <span className="font-light">
-                            Drag and drop or <span className="text-blue-500">browse</span> your files 
-                        </span>
-                    </button> 
+                    <>
+                        <button
+                            className="flex flex-col items-center justify-center font-medium text-lg"
+                            onClick={ handleClick }
+                        >
+                            <MdOutlineFileUpload className="text-5xl text-blue-500 animate-bounce" />
+                            <span className="font-light">
+                                Drag and drop or <span className="text-blue-500">browse</span> your files 
+                            </span>
+                        </button>
+                        <input
+                            type="file"
+                            onChange={ handleFileChange }
+                            ref={fileInputRef}
+                            className="hidden"
+                        />
+                    </>
                 )}
                 {isUploading && guidelinesFile === null && (
                     <span className="flex flex-col justify-center items-center gap-4">
@@ -49,15 +63,26 @@ export default function GuidelinesUpload() {
                     </span>
                 )}
                 {guidelinesFile !== null && (
-                    <span className="flex flex-col justify-center items-center gap-4">
-                        <span className="text-green-600 flex flex-row gap-1 items-center">
-                            <FaCheck />
-                            <span>guidelines.pdf</span>
-                        </span>
-                        <span className="font-light text-blue-500">
-                            Click to change file
-                        </span>
-                    </span>
+                    <>
+                        <button 
+                            className="flex flex-col justify-center items-center gap-4"
+                            onClick={ handleClick }
+                        >
+                            <span className="text-green-600 flex flex-row gap-1 items-center">
+                                <FaCheck />
+                                <span>guidelines.pdf</span>
+                            </span>
+                            <span className="font-light text-blue-500">
+                                Click to change file
+                            </span>
+                        </button>
+                        <input
+                            type="file"
+                            onChange={ handleFileChange }
+                            ref={fileInputRef}
+                            className="hidden"
+                        />
+                    </>
                 )}
             </div>
             <Toaster
--- frontend/components/medical-record-upload/index.tsx
+++ frontend/components/medical-record-upload/index.tsx
@@ -2,7 +2,7 @@
 
 import { useDashboard } from "@/context/dashboard-context";
 import classNames from "classnames";
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { FaCheck } from "react-icons/fa";
 import { MdOutlineFileUpload } from "react-icons/md";
 import Spinner from "@/components/dashboard/spinner";
@@ -10,8 +10,15 @@ import Spinner from "@/components/dashboard/spinner";
 export default function MedicalRecordUpload() {
     const { medicalRecord, setMedicalRecord } = useDashboard();
     const [isUploading, setIsUploading] = useState(false);
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
 
     const handleClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    }
+
+    const handleFileChange = () => {
         setIsUploading(true);
 
         setTimeout(() => {
@@ -25,15 +32,23 @@ export default function MedicalRecordUpload() {
             <h2 className="self-start font-medium text-xl">Upload Medical Record</h2>
             <div className="w-full h-full bg-gray-100 rounded-3xl border-4 border-black-500 border-dashed flex flex-row items-center justify-center">
                 {!isUploading && medicalRecord === null && (
-                    <button
-                        className="flex flex-col items-center justify-center font-medium text-lg"
-                        onClick={handleClick}
-                    >
-                        <MdOutlineFileUpload className="text-5xl text-blue-500 animate-bounce" />
-                        <span className="font-light">
-                            Drag and drop or <span className="text-blue-500">browse</span> your files 
-                        </span>
-                    </button> 
+                    <>
+                        <button
+                            className="flex flex-col items-center justify-center font-medium text-lg"
+                            onClick={handleClick}
+                        >
+                            <MdOutlineFileUpload className="text-5xl text-blue-500 animate-bounce" />
+                            <span className="font-light">
+                                Drag and drop or <span className="text-blue-500">browse</span> your files 
+                            </span>
+                        </button>
+                        <input
+                            type="file"
+                            onChange={ handleFileChange }
+                            ref={fileInputRef}
+                            className="hidden"
+                        />
+                    </>
                 )}
                 {isUploading && medicalRecord == null && (
                     <span className="flex flex-col justify-center items-center gap-4">
@@ -44,15 +59,26 @@ export default function MedicalRecordUpload() {
                     </span>
                 )}
                 {medicalRecord !== null && (
-                    <span className="flex flex-col justify-center items-center gap-4">
-                        <span className="text-green-600 flex flex-row gap-1 items-center">
-                            <FaCheck />
-                            <span>medical-record.pdf</span>
-                        </span>
-                        <span className="font-light text-blue-500">
-                            Click to change file
-                        </span>
-                    </span>
+                    <>
+                        <button 
+                            className="flex flex-col justify-center items-center gap-4"
+                            onClick={ handleClick }
+                        >
+                            <span className="text-green-600 flex flex-row gap-1 items-center">
+                                <FaCheck />
+                                <span>medical-record.pdf</span>
+                            </span>
+                            <span className="font-light text-blue-500">
+                                Click to change file
+                            </span>
+                        </button>
+                        <input
+                            type="file"
+                            onChange={ handleFileChange }
+                            ref={fileInputRef}
+                            className="hidden"
+                        />
+                    </>
                 )}
             </div>
         </div>
