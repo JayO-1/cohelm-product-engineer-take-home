71cc429efe10f7b68830b07de74fcb5639bc4c3e
JayO-1 <jozuzu1@yahoo.com>
2024-05-25 16:34:03 +0100

    Ex3.4: Added fallback page for case not found

--- frontend/app/dashboard/case/[case_id]/page.tsx
+++ frontend/app/dashboard/case/[case_id]/page.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { notFound } from 'next/navigation';
 import data from '@/resources/example-response.json';
 import Header from '@/components/dashboard/case-details/header';
 import StatusBar from '@/components/dashboard/case-details/status-bar';
 import Summary from '@/components/dashboard/case-details/summary';
 import StepWrapper from '@/components/dashboard/case-details/step-wrapper';
 
-export default function CaseResult() {
+export default function CaseResult({ params }: { params: { case_id: string } }) {
+	const id = params.case_id;
+
 	const procedureName = data.procedure_name;
 	const caseId = data.case_id;
 	const isMet = data.is_met;
 	const steps = data.steps;
 	const cptCodes = data.cpt_codes;
 	const summary = data.summary;
+
+	if (caseId !== id) {
+		notFound();
+	}
 
 	return (
 		<div className="w-full h-screen flex flex-col items-center gap-5 py-10">
